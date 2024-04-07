bf966d95b8858db00b3cfd451d071bc1a6e07ce0
Victory Deo <victory23tshimbadi@gmail.com>
2024-04-07 22:58:07 +0100

    add metadata

--- src/app/utils/types.ts
+++ src/app/utils/types.ts
@@ -5,3 +5,22 @@ export interface User {
   password?: string;
   confirmPassword?: string;
 }
+
+export interface IMetadata {
+  title: string;
+  description: string;
+  author?: string;
+  articleAuthor?: string;
+  publicationDate?: string;
+  articlePublishedTime?: string;
+  keywords?: string;
+  siteUrl?: string;
+  canonicalUrl: string;
+  articleModifiedTime?: string;
+  ogTwitterImage: string;
+  ogLocale?: string;
+  ogType: string;
+  ogImageUrl?: string;
+  ogUpdatedTime?: string;
+  children?: any;
+}
